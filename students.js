572577922848084d0cf051a018cccab6b9ae8682
cosmicbrownielover <emiliano101805@icloud.com>
2026-09-01 09:25:56 -0700

    Enhance discussion post with author details
    Added post author section with profile image and name.

--- students.js
+++ students.js
@@ -1,7 +1,27 @@
 /* Version 1 */
 let discussionContainer = document.getElementById("discussion-board");
 let discussionArticle = document.createElement("article");
-discussionArticle.className("discussion-post version-one");
-discussionArticle.idName("post-101");
+discussionArticle.className = "discussion-post, version-one";
+discussionArticle.id = "post-101";
 discussionContainer.appendChild(discussionArticle);
-let post
+let postAuthor = document.createElement("div");
+postAuthor.className = "post-author";
+discussionContainer.appendChild(postAuthor);
+let profileImage = document.createElement("img");
+profileImage.className = "profile-image";
+profileImage.src = "";
+profileImage.alt = "Profile Image";
+postAuthor.appendChild(profileImage);
+let authorInfo = document.createElement("div");
+authorInfo.className = "author-info";
+postAuthor.appendChild(authorInfo);
+let authorName = document.createElement("h2");
+authorName.className = "author-name";
+authorInfo.appendChild(authorName);
+let authorUsername = document.createElement("p");
+authorUsername.className = "author-username";
+authorInfo.appendChild(authorUsername);
+
+/* Version 2 */
+
+/* Version 3 */
